39d79a7a4341226c8772ee3f93d9455f766b7e9a
Jack Cable <jackhcable@gmail.com>
2017-12-05 22:00:13 -0600

    Changes to store and diff relative urls, file text + eslint

--- app/models/Domain.js
+++ app/models/Domain.js
@@ -1,23 +1,23 @@
-var Subdomain = require('./Subdomain');
-var paranoid = require("paranoid-request");
-var mongoose = require('mongoose');
-var jsdiff = require('diff');
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
+let Subdomain = require('./Subdomain');
+let paranoid = require("paranoid-request");
+let mongoose = require('mongoose');
+let jsdiff = require('diff');
+let AWS = require('aws-sdk');
+let s3 = new AWS.S3();
 
 const DomainSchema = new mongoose.Schema({
-    name : { type : String, default: '', index: true, unique: true },
-    url : { type : String, default: '' },
+    name: { type : String, default: '', index: true, unique: true },
+    url: { type : String, default: '' },
     subdomains: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Subdomain' }],
     files: [{ type: mongoose.Schema.Types.ObjectId, ref: 'File' }]
 });
 
-DomainSchema.pre('save', function(next) {
+DomainSchema.pre('save', (next) => {
     next();
 });
 
-DomainSchema.path('name').validate(function(name) {
-  if (name == '') return false;
+DomainSchema.path('name').validate((name) => {
+  if (name === '') return false;
   return true;
 }, 'The name is not valid.');
 
@@ -27,33 +27,62 @@ DomainSchema.methods = {
     },
     // returns a promise of the contents of the locally stored file
     getLocalContents(error, success) {
-        s3.getObject({
-            Bucket: process.env.AWS_BUCKET_NAME,
-            Key: this.getFilteredFileUrl(),
-        }, (err, data) => {
-            if (err) return error(err);
-            success(data.Body.toString('utf-8'));
+        this.bulkReadFromBucket([{
+            key: this.getFilteredFileUrl()
+        }, {
+            key: 'diff-' + this.getFilteredFileUrl()
+        }, {
+            key: 'urls-' + this.getFilteredFileUrl()
+        }, {
+            key: 'urls-diff-' + this.getFilteredFileUrl()
+        }], (err) => {
+            error(err);
+        }, (data) => {
+            success(data);
         });
     },
-    // is this function name too long? maybe... but it does what it says it does
-    // writes the file to the bucket and extracts urls, writes those to the bucket
-    writeToBucketAndExtractRelativeUrls(data, err, success) {
+    // returns contents of arrays of files
+    bulkReadFromBucket(keyArray, error, success) {
+        let keyObject = keyArray.pop();
+        s3.getObject({
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: keyObject.key,
+        }, (err, response) => {
+            let responseConstruct = {};
+            if (response && response.Body) {
+                responseConstruct = response.Body.toString('utf8');
+            }
+            if (err || !response || !response.Body) {
+                responseConstruct = { error: err }; //fail silently
+            }
+            if (keyArray.length > 0) {
+                this.bulkReadFromBucket(keyArray, error, (responseArray) => {
+                    success(responseArray.concat([responseConstruct]))
+                });
+            } else {
+                success([responseConstruct]);
+            }
+        });
+    },
+    // writes an array of multiple data objects to the bucket
+    bulkWriteToBucket(dataArray, error, success) {
+        let dataObject = dataArray.pop();
         s3.putObject({
             Bucket: process.env.AWS_BUCKET_NAME,
-            Key: this.getFilteredFileUrl(),
-            Body: data
+            Key: dataObject.key,
+            Body: dataObject.data
         }, (err, response) => {
-            if (err) return error(err);
-            let urls = this.extractRelativeUrls(data);
-            s3.putObject({
-                Bucket: process.env.AWS_BUCKET_NAME,
-                Key: 'urls-' + this.getFilteredFileUrl(),
-                Body: urls.join('\n')
-            }, (err, data) => {
-                if (err) return error(err);
-                console.log("The file was saved!");
-                success();
-            });
+            let responseConstruct = response;
+            if (err) {
+                responseConstruct = { error: err }; //fail silently
+            }
+            if (dataArray.length > 0) {
+                this.bulkWriteToBucket(dataArray, error, (responseArray) => {
+                    success(responseArray.concat(responseConstruct))
+                });
+            } else {
+                success([responseConstruct]);
+            }
         });
     },
     extractRelativeUrls(data) {
@@ -73,16 +102,24 @@ DomainSchema.methods = {
         paranoid.get(this.url, (err, res, newData) => {
             if (err) return error(err);
             if (isNew) {
-                return this.writeToBucketAndExtractRelativeUrls(newData, (err) => {
+                return this.bulkWriteToBucket([{
+                    data: newData,
+                    key: this.getFilteredFileUrl()
+                }, {
+                    data: this.extractRelativeUrls(newData).join('\n'),
+                    key: 'urls-' + this.getFilteredFileUrl()
+                }], (err) => {
+                    console.log(err);
                     return error(err);
-                }, () => {
+                }, (newData) => {
                     console.log("The file was saved!");
                     success(newData);
                 });
             }
             this.getLocalContents((err) => {
                 return error(err);
-            }, (originalData) => {
+            }, (originalDataArray) => {
+                let originalData = originalDataArray[0] //TODO: check for error here
                 if (newData == originalData) { // file has not been modified, return
                     console.log('File has not been modified.');
                     return success(originalData);
@@ -104,23 +141,28 @@ DomainSchema.methods = {
                     }
                 }
 
-                if (modifications.length && numCharsModified / originalData.length > threshold) {
+                let originalUrls = originalDataArray[2]; // location of the url file
+                let newUrls = this.extractRelativeUrls(newData).join('\n')
+                let urlsDiff = jsdiff.diffChars(originalUrls, newUrls);
+                if (modifications.length && numCharsModified > threshold) {
                     console.log("Modifications made, saving files.");
-                    s3.putObject({
-                        Bucket: process.env.AWS_BUCKET_NAME,
-                        Key: this.getFilteredFileUrl(),
-                        Body: newData
-                    }, (err, data) => {
-                        if (err) return error(err);
-                        s3.putObject({
-                            Bucket: process.env.AWS_BUCKET_NAME,
-                            Key: 'diff-' + this.getFilteredFileUrl(),
-                            Body: JSON.stringify(diff)
-                        }, (err, data) => {
-                            if (err) return error(err);
-                            console.log("The file was saved!");
-                            success(newData);
-                        });
+                    this.bulkWriteToBucket([{
+                        data: newData,
+                        key: this.getFilteredFileUrl()
+                    }, {
+                        data: JSON.stringify(diff),
+                        key: 'diff-' + this.getFilteredFileUrl()
+                    }, {
+                        data: newUrls,
+                        key: 'urls-' + this.getFilteredFileUrl()
+                    }, {
+                        data: JSON.stringify(urlsDiff),
+                        key: 'urls-diff-' + this.getFilteredFileUrl()
+                    }], (err) => {
+                        return error(err);
+                    }, (response) => {
+                        console.log("The file was saved!");
+                        success(response);
                     });
                 }
             });
